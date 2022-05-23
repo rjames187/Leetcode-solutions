692fc0f130fc6510819d291491e5f1811897921f
Rory James <84684031+rjames187@users.noreply.github.com>
2022-05-23 17:55:53 -0500

    Time: 81 ms (31.19%), Space: 14.4 MB (97.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(t) == sorted(s)
+        
+        
+        hash_table_s = {}
+        for i in s:
+            if i in hash_table_s:
+                hash_table_s[i] += 1
+            else:
+                hash_table_s[i] = 1
+        
+        hash_table_t = {}
+        for i in t:
+            if i in hash_table_t:
+                hash_table_t[i] += 1
+            else:
+                hash_table_t[i] = 1
+        
+        return hash_table_s == hash_table_t
+               
