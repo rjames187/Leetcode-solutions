ded5254fe5e4d87285806411344a755ee356bebb
Rory James <84684031+rjames187@users.noreply.github.com>
2022-07-31 20:18:02 -0500

    Time: 139 ms (38.07%), Space: 15.4 MB (14.02%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_table = {}
-        for i, x in enumerate(nums):
-            hash_table[x] = i
+        for i in range(len(nums)):
+            hash_table[target - nums[i]] = i
         
-        for i, x in enumerate(nums):
-            y = target - x
-            if y in hash_table:
-                j = hash_table[y]
-                if i != j:
-                    return [i, j]
+        for j in range(len(nums)):
+            if nums[j] in hash_table and hash_table[nums[j]] != j:
+                return [hash_table[nums[j]], j]
+                
+            
