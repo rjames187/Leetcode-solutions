b13195bff89623ec70d6087b35bf0b4575749b8f
Rory James <84684031+rjames187@users.noreply.github.com>
2022-09-28 18:21:50 -0500

    Time: 41 ms (98.17%), Space: 14.8 MB (30.74%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,16 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = list(filter(lambda ch: ch.isalnum(), s))
+        s = ''.join(s)
         s = s.lower()
-        s = list(filter(lambda i: (i.isalnum()), s))
-        print(s)
-        # two pointers solution
-        i = 0
-        j = len(s) - 1
-        
-        while i < j:
-            if s[i] != s[j]:
-                return False
-            i += 1
-            j -= 1
-        
-        return True
+        return s == s[::-1]
