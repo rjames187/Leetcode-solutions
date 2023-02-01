9fd6a5cdb271088b58233481cce44ad85bbe85bb
Rory James <84684031+rjames187@users.noreply.github.com>
2023-02-01 15:02:07 -0600

    Time: 101 ms (99.35%), Space: 53.5 MB (40.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,25 +2,20 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var addToMap = (indexMap, normWord, originalWord) => {
-    if (indexMap.has(normWord)) {
-        const curVal = indexMap.get(normWord)
-        indexMap.set(normWord, curVal.concat([originalWord]))
-    } else {
-        indexMap.set(normWord, [originalWord])
-    }
-}
 
 var groupAnagrams = function(strs) {
-    let indexMap = new Map()
+    const map = new Map()
     
-    for (let i = 0; i < strs.length; i++) {
-        let originalWord = strs[i]
-        let normWord = originalWord.split('')
-        normWord.sort()
-        normWord = normWord.join('')
-        addToMap(indexMap, normWord, originalWord)
+    for (let str of strs) {
+        const key = reorder(str)
+        const vals = map.get(key) || []
+        vals.push(str)
+        map.set(key, vals)
     }
     
-    return Array.from(indexMap.values())
-};
+    return Array.from(map.values())
+}
+
+var reorder = function(str) {
+    return str.split('').sort().join('')
+}
