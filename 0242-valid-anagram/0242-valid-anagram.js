a351e1c04028f7014352bf946acd6a32ec1c042f
Rory James <84684031+rjames187@users.noreply.github.com>
2023-02-01 14:35:26 -0600

    Time: 116 ms (35.85%), Space: 49.5 MB (8.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,38 +4,12 @@
  * @return {boolean}
  */
 
-
+var reorder = function(str) {
+    return str.split('').sort().join('')
+}
 
 var isAnagram = function(s, t) {
-    let freqMap = new Map()
+    return reorder(s) === reorder(t)
+}
 
-    var logOcc = function(chr, sign) {
-        if (freqMap.has(chr)) {
-            var curVal = freqMap.get(chr)
-            freqMap.set(chr, curVal + sign)
-            return
-        }
-        freqMap.set(chr, sign)
-    }
-    
-    var onlyZeros = function(it) {
-        for (i of it) {
-            if (i !== 0) {
-                return false
-            }
-        }
-        return true
-    }
-    
-    if (s.length !== t.length) { return false }
-    for (let i = 0; i < s.length; i++) {
-        logOcc(s[i], 1)
-        logOcc(t[i], -1)
-    }
-    
-    const mapIter = freqMap.values()
-    return onlyZeros(mapIter)
-};
-
-// time: O(N)
 
