9b6669c7210eec5bec7032503d180b9794965f09
Rory James <84684031+rjames187@users.noreply.github.com>
2023-02-11 18:53:31 -0600

    Time: 104 ms (27.40%), Space: 52 MB (18.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0
-    
-    let left = 0, right = 1
-    
-    while (right < prices.length) {
-        const profit = prices[right] - prices[left]
-        
-        maxProfit = Math.max(maxProfit, profit)
-        
-        if (profit < 0) left = right
-        right++
-    }
+   let left = 0
+   let right = 1
+   let maxProfit = 0
+   
+   while (right < prices.length) {
+       const profit = prices[right] - prices[left]
+       
+       if (profit < 0) left = right
+       else maxProfit = Math.max(maxProfit, profit)
+       
+       right++
+   }
     
     return maxProfit
 }
