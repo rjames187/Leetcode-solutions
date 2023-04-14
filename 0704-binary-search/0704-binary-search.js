12d4e38eb906d49035ed8acb20d26f3e74910c05
Rory James <84684031+rjames187@users.noreply.github.com>
2023-04-13 21:04:29 -0500

    Time: 65 ms (56.65%), Space: 45.2 MB (34.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -10,14 +10,10 @@ var search = function(nums, target) {
     while (left <= right) {
         const mid = Math.floor((left + right) / 2)
         
-        if (nums[mid] === target) {
-            return mid
-        } else if (nums[mid] > target) {
-            right = mid - 1
-        } else {
-            left = mid + 1
-        }
+        if (nums[mid] === target) return mid
+        if (nums[mid] > target) right = mid - 1
+        else left = mid + 1
     }
     
     return -1
-};
+}
