a3e3737c2fb2d6f30e374eda583cc1e49642ee18
Rory James <84684031+rjames187@users.noreply.github.com>
2023-05-07 22:38:45 -0500

    Time: 62 ms (33.33%), Space: 42.1 MB (47.49%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,8 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const res = []
     for (let i = 0; i < arr.length; i++) {
-        res.push(fn(arr[i], i))
+        arr.splice(i, 1, fn(arr[i], i))
     }
-    return res
+    return arr
 };
