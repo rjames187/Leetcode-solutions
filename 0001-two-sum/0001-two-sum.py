62a36a29e8f5d12865caa4d1fd05937233df84b2
Rory James <84684031+rjames187@users.noreply.github.com>
2023-08-17 00:09:55 -0500

    Time: 64 ms (87.73%), Space: 17.7 MB (14.50%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i, x in enumerate(nums):
+            hash_table[x] = i
+        
+        for i, x in enumerate(nums):
+            y = target - x
+            if y in hash_table:
+                j = hash_table[y]
+                if i != j:
+                    return [i, j]
